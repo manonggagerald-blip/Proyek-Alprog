9d1900b12826039d882331ef6c2436f9a29353ad
potalangiexcel-da <potalangiexcel@gmail.com>
2026-06-02 11:45:24 +0800

    Tes kode
    Tes kode bahasa c

--- Main.c
+++ Main.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main(){
+int n;
+
+printf("Masukkan nilai n: ");
+scanf("%d",&n);
+
+int arr[n];
+printf("Masukkan data array: ");
+for(int i=0;i<n;i++){
+scanf("%d",&arr[i]);
+printf("%d",arr[i]);
+}
+
+
+return 0;  
+}
